0f3d6934a212f7736e16177ad541dab7042a4557
sdac-7 <sdac-7@sdac-7-PC>
2024-11-25 12:39:16 +0530

    consumer

---
+++ PBYC2/src/account/Account.java
@@ -0,0 +1,14 @@
+package account;
+
+public class Account extends company.Company{
+	@Override
+	public double calSal(double sal) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public double calSal(double sal, double tax) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
